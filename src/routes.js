9e615e58412798f228b531240880983b1cce83e6
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-06-29 18:01:33 -0300

    Início da criação da estrutura de dados

--- src/routes.js
+++ src/routes.js
@@ -10,10 +10,26 @@ const profile = {
     'vacation-per-year': 4
 }
 
+const jobs = [];
+
 const views = __dirname + "/views/";
 
 routes.get('/', (req, res) => res.render(views + "index"));
 routes.get('/job', (req, res) => res.render(views + "job"));
+routes.post('/job', (req, res) => {
+    // req.body = { name: 'Nada', 'daily-hours': '4', 'total-hours': '5' }
+
+    const lastId = jobs[jobs.length -1]?.id || 1;
+
+    jobs.push({
+        id: lastId +1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now(),
+    });
+    return res.redirect('/');
+});
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
