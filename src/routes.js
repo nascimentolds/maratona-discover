7411cdb88427739d7ed8ebcd5803a99770b25d02
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-06-30 17:19:17 -0300

    exibindo jobs

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,53 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
 const profile = {
-    name: 'Marcelo Nascimento',
-    avatar: 'https://github.com/nascimentolds.png',
-    'monthly-budget': 3000,
-    'days-per-week': 5,
-    'hours-per-day': 5,
-    'vacation-per-year': 4
-}
+  name: "Marcelo Nascimento",
+  avatar: "https://github.com/nascimentolds.png",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4,
+};
 
-const jobs = [];
+const jobs = [
+  {
+    id: 1,
+    name: "Pizzaria Guloso",
+    "daily-hours": 2,
+    "total-hours": 60,
+    created_at: Date.now(),
+  },
+  {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 3,
+    "total-hours": 47,
+    created_at: Date.now(),
+  },
+];
 
 const views = __dirname + "/views/";
 
-routes.get('/', (req, res) => res.render(views + "index"));
-routes.get('/job', (req, res) => res.render(views + "job"));
-routes.post('/job', (req, res) => {
-    // req.body = { name: 'Nada', 'daily-hours': '4', 'total-hours': '5' }
+routes.get("/", (req, res) => res.render(views + "index", { jobs }));
+routes.get("/job", (req, res) => res.render(views + "job"));
+routes.post("/job", (req, res) => {
+  // req.body = { name: 'Nada', 'daily-hours': '4', 'total-hours': '5' }
 
-    const lastId = jobs[jobs.length -1]?.id || 1;
+  const lastId = jobs[jobs.length - 1]?.id || 1;
 
-    jobs.push({
-        id: lastId +1,
-        name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
-        created_at: Date.now(),
-    });
-    return res.redirect('/');
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    created_at: Date.now(),
+  });
+  return res.redirect("/");
 });
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
+routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
+routes.get("/profile", (req, res) =>
+  res.render(views + "profile", { profile })
+);
 
 module.exports = routes;
